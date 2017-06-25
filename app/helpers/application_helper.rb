80b5faebd1b9d1a70821be6a755757f9e65e53fa
Ben Soutendijk <bensoutendijk@gmail.com>
2017-06-25 22:10:24 +0000

    played with flash styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
 end
+def flash_class(level)
+    case level
+        when 'notice' then "container alert alert-dismissable alert-info"
+        when 'success' then "container alert alert-dismissable alert-success"
+        when 'error' then "container alert alert-dismissable alert-danger"
+        when 'alert' then "container alert alert-dismissable alert-danger"
+    end
+end
