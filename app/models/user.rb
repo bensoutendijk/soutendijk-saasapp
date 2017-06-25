35a914552ff2821ac7ec714ae6b6f7c820401aa7
Ben Soutendijk <bensoutendijk@gmail.com>
2017-06-25 20:56:24 +0000

    added code comments for Stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   belongs_to :plan
   
   attr_accessor :stripeToken
+  
+  # If pro user passes validations
+  # then call call Stripe and tell Stripe to set up a subscription
+  # upon charging the user's credit card
+  # Stripe responds back with customer data
+  # and we store customer id as the customer token
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(
